e488aed2c94df64757ba25266888483493594305
Ro <Ro@->
2024-06-23 14:40:59 +0300

    Добавьте файлы проекта.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Calc
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_result = new System.Windows.Forms.Label();
+            this.button_0 = new System.Windows.Forms.Button();
+            this.button_1 = new System.Windows.Forms.Button();
+            this.button_2 = new System.Windows.Forms.Button();
+            this.button_3 = new System.Windows.Forms.Button();
+            this.button_6 = new System.Windows.Forms.Button();
+            this.button_5 = new System.Windows.Forms.Button();
+            this.button_4 = new System.Windows.Forms.Button();
+            this.button_9 = new System.Windows.Forms.Button();
+            this.button_8 = new System.Windows.Forms.Button();
+            this.button_7 = new System.Windows.Forms.Button();
+            this.button_plus = new System.Windows.Forms.Button();
+            this.button_minus = new System.Windows.Forms.Button();
+            this.button_mult = new System.Windows.Forms.Button();
+            this.button_div = new System.Windows.Forms.Button();
+            this.button_equ = new System.Windows.Forms.Button();
+            this.button_CE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_result
+            // 
+            this.label_result.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label_result.Font = new System.Drawing.Font("Montserrat", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_result.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label_result.Location = new System.Drawing.Point(12, 24);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(385, 67);
+            this.label_result.TabIndex = 0;
+            this.label_result.Text = "123451+684";
+            this.label_result.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button_0
+            // 
+            this.button_0.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_0.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_0.Location = new System.Drawing.Point(12, 467);
+            this.button_0.Name = "button_0";
+            this.button_0.Size = new System.Drawing.Size(68, 61);
+            this.button_0.TabIndex = 1;
+            this.button_0.Text = "0";
+            this.button_0.UseVisualStyleBackColor = false;
+            // 
+            // button_1
+            // 
+            this.button_1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_1.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_1.Location = new System.Drawing.Point(12, 390);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(68, 61);
+            this.button_1.TabIndex = 2;
+            this.button_1.Text = "1";
+            this.button_1.UseVisualStyleBackColor = false;
+            // 
+            // button_2
+            // 
+            this.button_2.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_2.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_2.Location = new System.Drawing.Point(103, 390);
+            this.button_2.Name = "button_2";
+            this.button_2.Size = new System.Drawing.Size(68, 61);
+            this.button_2.TabIndex = 3;
+            this.button_2.Text = "2";
+            this.button_2.UseVisualStyleBackColor = false;
+            // 
+            // button_3
+            // 
+            this.button_3.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_3.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_3.Location = new System.Drawing.Point(196, 390);
+            this.button_3.Name = "button_3";
+            this.button_3.Size = new System.Drawing.Size(68, 61);
+            this.button_3.TabIndex = 4;
+            this.button_3.Text = "3";
+            this.button_3.UseVisualStyleBackColor = false;
+            // 
+            // button_6
+            // 
+            this.button_6.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_6.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_6.Location = new System.Drawing.Point(196, 306);
+            this.button_6.Name = "button_6";
+            this.button_6.Size = new System.Drawing.Size(68, 61);
+            this.button_6.TabIndex = 7;
+            this.button_6.Text = "6";
+            this.button_6.UseVisualStyleBackColor = false;
+            // 
+            // button_5
+            // 
+            this.button_5.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_5.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_5.Location = new System.Drawing.Point(103, 306);
+            this.button_5.Name = "button_5";
+            this.button_5.Size = new System.Drawing.Size(68, 61);
+            this.button_5.TabIndex = 6;
+            this.button_5.Text = "5";
+            this.button_5.UseVisualStyleBackColor = false;
+            // 
+            // button_4
+            // 
+            this.button_4.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_4.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_4.Location = new System.Drawing.Point(12, 306);
+            this.button_4.Name = "button_4";
+            this.button_4.Size = new System.Drawing.Size(68, 61);
+            this.button_4.TabIndex = 5;
+            this.button_4.Text = "4";
+            this.button_4.UseVisualStyleBackColor = false;
+            // 
+            // button_9
+            // 
+            this.button_9.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_9.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_9.Location = new System.Drawing.Point(196, 224);
+            this.button_9.Name = "button_9";
+            this.button_9.Size = new System.Drawing.Size(68, 61);
+            this.button_9.TabIndex = 10;
+            this.button_9.Text = "9";
+            this.button_9.UseVisualStyleBackColor = false;
+            // 
+            // button_8
+            // 
+            this.button_8.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_8.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_8.Location = new System.Drawing.Point(103, 224);
+            this.button_8.Name = "button_8";
+            this.button_8.Size = new System.Drawing.Size(68, 61);
+            this.button_8.TabIndex = 9;
+            this.button_8.Text = "8";
+            this.button_8.UseVisualStyleBackColor = false;
+            // 
+            // button_7
+            // 
+            this.button_7.BackColor = System.Drawing.Color.LightSalmon;
+            this.button_7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_7.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_7.Location = new System.Drawing.Point(12, 224);
+            this.button_7.Name = "button_7";
+            this.button_7.Size = new System.Drawing.Size(68, 61);
+            this.button_7.TabIndex = 8;
+            this.button_7.Text = "7";
+            this.button_7.UseVisualStyleBackColor = false;
+            // 
+            // button_plus
+            // 
+            this.button_plus.BackColor = System.Drawing.Color.Chocolate;
+            this.button_plus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_plus.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_plus.Location = new System.Drawing.Point(293, 224);
+            this.button_plus.Name = "button_plus";
+            this.button_plus.Size = new System.Drawing.Size(104, 61);
+            this.button_plus.TabIndex = 11;
+            this.button_plus.Text = "+";
+            this.button_plus.UseVisualStyleBackColor = false;
+            this.button_plus.Click += new System.EventHandler(this.button_plus_Click);
+            // 
+            // button_minus
+            // 
+            this.button_minus.BackColor = System.Drawing.Color.Chocolate;
+            this.button_minus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_minus.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_minus.Location = new System.Drawing.Point(293, 306);
+            this.button_minus.Name = "button_minus";
+            this.button_minus.Size = new System.Drawing.Size(104, 61);
+            this.button_minus.TabIndex = 12;
+            this.button_minus.Text = "-";
+            this.button_minus.UseVisualStyleBackColor = false;
+            this.button_minus.Click += new System.EventHandler(this.button_minus_Click);
+            // 
+            // button_mult
+            // 
+            this.button_mult.BackColor = System.Drawing.Color.Chocolate;
+            this.button_mult.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_mult.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_mult.Location = new System.Drawing.Point(293, 390);
+            this.button_mult.Name = "button_mult";
+            this.button_mult.Size = new System.Drawing.Size(104, 61);
+            this.button_mult.TabIndex = 13;
+            this.button_mult.Text = "*";
+            this.button_mult.UseVisualStyleBackColor = false;
+            this.button_mult.Click += new System.EventHandler(this.button_mult_Click);
+            // 
+            // button_div
+            // 
+            this.button_div.BackColor = System.Drawing.Color.Chocolate;
+            this.button_div.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_div.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_div.Location = new System.Drawing.Point(293, 467);
+            this.button_div.Name = "button_div";
+            this.button_div.Size = new System.Drawing.Size(104, 61);
+            this.button_div.TabIndex = 14;
+            this.button_div.Text = "/";
+            this.button_div.UseVisualStyleBackColor = false;
+            this.button_div.Click += new System.EventHandler(this.button_div_Click);
+            // 
+            // button_equ
+            // 
+            this.button_equ.BackColor = System.Drawing.Color.Bisque;
+            this.button_equ.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_equ.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_equ.Location = new System.Drawing.Point(118, 467);
+            this.button_equ.Name = "button_equ";
+            this.button_equ.Size = new System.Drawing.Size(134, 61);
+            this.button_equ.TabIndex = 15;
+            this.button_equ.Text = "=";
+            this.button_equ.UseVisualStyleBackColor = false;
+            this.button_equ.Click += new System.EventHandler(this.button_equ_Click);
+            // 
+            // button_CE
+            // 
+            this.button_CE.BackColor = System.Drawing.Color.Bisque;
+            this.button_CE.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_CE.Font = new System.Drawing.Font("Montserrat", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_CE.Location = new System.Drawing.Point(306, 127);
+            this.button_CE.Name = "button_CE";
+            this.button_CE.Size = new System.Drawing.Size(91, 73);
+            this.button_CE.TabIndex = 16;
+            this.button_CE.Text = "CE";
+            this.button_CE.UseVisualStyleBackColor = false;
+            this.button_CE.Click += new System.EventHandler(this.button_CE_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(417, 540);
+            this.Controls.Add(this.button_CE);
+            this.Controls.Add(this.button_equ);
+            this.Controls.Add(this.button_div);
+            this.Controls.Add(this.button_mult);
+            this.Controls.Add(this.button_minus);
+            this.Controls.Add(this.button_plus);
+            this.Controls.Add(this.button_9);
+            this.Controls.Add(this.button_8);
+            this.Controls.Add(this.button_7);
+            this.Controls.Add(this.button_6);
+            this.Controls.Add(this.button_5);
+            this.Controls.Add(this.button_4);
+            this.Controls.Add(this.button_3);
+            this.Controls.Add(this.button_2);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.button_0);
+            this.Controls.Add(this.label_result);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.Button button_0;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_6;
+        private System.Windows.Forms.Button button_5;
+        private System.Windows.Forms.Button button_4;
+        private System.Windows.Forms.Button button_9;
+        private System.Windows.Forms.Button button_8;
+        private System.Windows.Forms.Button button_7;
+        private System.Windows.Forms.Button button_plus;
+        private System.Windows.Forms.Button button_minus;
+        private System.Windows.Forms.Button button_mult;
+        private System.Windows.Forms.Button button_div;
+        private System.Windows.Forms.Button button_equ;
+        private System.Windows.Forms.Button button_CE;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class MainForm : Form
+    {
+        enum operation {empty, plus, minus, multiply, devide };
+        operation oper=operation.empty;
+        double number;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            button_0.Click += NumberButton_Click;
+            button_1.Click += NumberButton_Click;
+            button_2.Click += NumberButton_Click;
+            button_3.Click += NumberButton_Click;
+            button_4.Click += NumberButton_Click;
+            button_5.Click += NumberButton_Click;
+            button_6.Click += NumberButton_Click;
+            button_7.Click += NumberButton_Click;
+            button_8.Click += NumberButton_Click;
+            button_9.Click += NumberButton_Click;
+          
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            label_result.Text = String.Empty;
+        }
+
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            Button button=(Button)sender;
+            label_result.Text += button.Text;
+        }
+
+        private void Check_Operation(operation oper, ref double number)
+        {
+            if(oper==operation.empty)
+            {
+                number = double.Parse(label_result.Text);
+            }
+            else
+            {
+                if(oper == operation.plus)
+                {
+                    number += double.Parse(label_result.Text);
+                }
+                else if (oper == operation.minus)
+                {
+                    number -= double.Parse(label_result.Text);
+                }
+                else if (oper == operation.multiply)
+                {
+                    number *= double.Parse(label_result.Text);
+                }
+                else
+                {
+                    number /= double.Parse(label_result.Text);
+                }
+            }
+        }
+
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {           
+            Check_Operation(oper, ref number);
+            label_result.Text = String.Empty;
+            oper = operation.plus;
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            Check_Operation(oper, ref number);
+            label_result.Text = String.Empty;
+            oper = operation.minus;
+        }
+
+        private void button_mult_Click(object sender, EventArgs e)
+        {
+            Check_Operation(oper, ref number);
+            label_result.Text = String.Empty;
+            oper = operation.multiply;
+        }
+
+        private void button_div_Click(object sender, EventArgs e)
+        {
+            Check_Operation(oper, ref number);
+            label_result.Text = String.Empty;
+            oper = operation.devide;
+        }
+
+        private void button_equ_Click(object sender, EventArgs e)
+        {
+            Check_Operation(oper, ref number);
+            oper = operation.empty;
+            label_result.Text=number.ToString();
+        }
+
+        private void button_CE_Click(object sender, EventArgs e)
+        {
+            number = 0;
+            label_result.Text = String.Empty;
+            oper = operation.empty;
+        }
+
+      
+    }
+}
